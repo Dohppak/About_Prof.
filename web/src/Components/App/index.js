613b0f1bcb35819448aecee1e6633ddd0495a870
Dohppak <seung20764298@gmail.com>
2018-12-30 15:01:56 +0900

    for pull

--- web/src/Components/App/index.js
+++ web/src/Components/App/index.js
@@ -11,7 +11,7 @@ class App extends Component {
     return (
       <Router>
         <StoreProvider>
-          <div className="App">
+          <div className={styles.app}>
             <Route 
                 exact path='/'
                 render={Intro}
