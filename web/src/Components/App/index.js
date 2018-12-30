acb9ccf95dfafc16c3edbb2945b9673b24e6b0cd
Sangkwun <wer2774@gmail.com>
2018-12-30 15:00:23 +0900

    Add mode selector

--- web/src/Components/App/index.js
+++ web/src/Components/App/index.js
@@ -14,7 +14,7 @@ class App extends Component {
           <div className="App">
             <Route 
                 exact path='/'
-                render={Intro}
+                component={Intro}
               />
             <Route
               path='/result'
--- web/src/Components/Intro/index.js
+++ web/src/Components/Intro/index.js
@@ -2,45 +2,60 @@ import React, { Component } from 'react';
 import Search from '../Search';
 import styles from './index.scss';
 
+const Selector = (props) => {
+    return(
+        <div className={styles.typeSelecter}>
+            <h4 id={1} onClick={props.changeMode}>professor</h4>
+            <h4 id={2} onClick={props.changeMode}>keyword</h4>
+        </div>
+    )
+}
+
 class Intro extends Component {
     constructor(props) {
         super(props);
         this.state = {
             keyword: false
+        };
+    }
+    changeMode = (e) => {
+        let  keyword
+        if(e.target.id === '1'){
+            keyword = false
+        }else{
+            keyword = true
         }
+        this.setState({
+            keyword
+        })
+        console.log(this.state.keyword)
     }
     render(){
         return(
             <div calssName={styles.body}>
-            <div className={styles.header}>
-                <div className={styles.logo}>
-                    <h1>About Prof.</h1>
-                </div>
+                <div className={styles.header}>
+                    <div className={styles.logo}>
+                        <h1>About Prof.</h1>
+                    </div>
 
-                <div calssName={styles.intro}>
-                    <h3>You can search Prof's Research field in UNIST</h3>
+                    <div calssName={styles.intro}>
+                        <h3>You can search Prof's Research field in UNIST</h3>
+                    </div>
                 </div>
-         </div>
-
-            <div className={styles.introbox}>
-                <Search/>
-            </div>
-
-            <div className={styles.footer}>
-                <div className={styles.typeSelecter}>
-                    <h4>professor</h4>
-                    <h4>keyword</h4>
+                <div className={styles.introbox}>
+                    <Search keyword={this.state.keyword}/>
                 </div>
-                <div calssName={styles.aboutus}>
-                    <h4>
-                        aboutus
-                    </h4>
+                <div className={styles.footer}>
+                    <Selector changeMode={this.changeMode}/>
+                    <div calssName={styles.aboutus}>
+                        <h4>
+                            aboutus
+                        </h4>
+                    </div>
                 </div>
-            </div>
         </div>
         )
     }
-    
 }
 
 export default Intro
